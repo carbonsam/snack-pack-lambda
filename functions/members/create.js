eaca1a05a29f3dfb76d6a84fa0d8b544126ab189
Samuel Schumacher <samschu@live.com>
2019-06-03 19:24:31 -0500

    Add function to get a specific member (#3)

--- functions/members/create.js
+++ functions/members/create.js
@@ -1,15 +1,14 @@
-import uuid from 'uuid';
 import * as dynamoDb from '../../utilities/dynamodb';
 import { responseSuccess, responseError } from '../../utilities/responses';
 
-export const createMember = async event => {
+export const main = async event => {
   const eventBody = JSON.parse(event.body);
-
   const dbParams = {
     TableName: 'snack_pack_members',
     Item: {
       memberId: eventBody.memberId, // unique member id
       organizationId: eventBody.organizationId, // unique id for the member's current organization
+      currentPackId: undefined,
       preferences: {
         availableWeekDays: eventBody.availableWeekDays || [], // Mon-Fri, e.g. '0'
         availableLunchTimes: eventBody.availableLunchTimes || [] // hour slots for lunches, e.g. '11:00-12:00'
@@ -24,7 +23,6 @@ export const createMember = async event => {
       await dynamoDb.call('put', dbParams);
       return responseSuccess(dbParams.Item);
     } else {
-      console.log('Error - Missing Required Parameter');
       return responseError({
         status: false,
         message: 'missing required parameter'
